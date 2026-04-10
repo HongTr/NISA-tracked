f1d30a4a908ca93fc3153b6f29e0fbdc3784c3d4
HongTr <truongminhhong0306@gmail.com>
2026-04-10 16:52:50 +0900

    fixed

--- server.py
+++ server.py
@@ -1,10 +1,6 @@
 from flask import Flask, jsonify
 from flask_cors import CORS
 import os
-from dotenv import load_dotenv
-
-# Nạp biến môi trường
-load_dotenv()
 
 app = Flask(__name__)
 
@@ -20,5 +16,5 @@ def health():
     return jsonify({"status": "ok"}), 200
 
 if __name__ == '__main__':
-    port = int(os.getenv('PORT', 5000))
+    port = int(os.environ.get('PORT', 5000))
     app.run(host='0.0.0.0', port=port, debug=False)
